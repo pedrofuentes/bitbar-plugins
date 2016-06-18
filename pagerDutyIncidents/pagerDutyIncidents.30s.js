0c2d08e7418d20edf4d584b24e9fa285d6bda0d6
Pedro Fuentes <pedro@groupon.com>
2016-06-18 14:47:42 -0700

    remove unnecessary brackets

--- pagerDutyIncidents/pagerDutyIncidents.30s.js
+++ pagerDutyIncidents/pagerDutyIncidents.30s.js
@@ -65,12 +65,12 @@ fetch(`${config.api.endpoint}/api/v1/incidents?${config.api.query}`, {
     Authorization: `Token token=${config.api.token}`,
   },
 })
-.then((res) => res.json())
-.then((json) => {
+.then(res => res.json())
+.then(json => {
   // Group incidents by Service
   const serviceIncidents = [];
 
-  json.incidents.forEach((incident) => {
+  json.incidents.forEach(incident => {
     if (!serviceIncidents[incident.service.id]) {
       serviceIncidents[incident.service.id] = {
         name: incident.service.name,
@@ -87,7 +87,7 @@ fetch(`${config.api.endpoint}/api/v1/incidents?${config.api.query}`, {
     total: json.incidents.length,
   };
 })
-.then((obj) => {
+.then(obj => {
   const json = [];
   const serviceIncidents = obj.serviceIncidents;
 
@@ -103,10 +103,10 @@ fetch(`${config.api.endpoint}/api/v1/incidents?${config.api.query}`, {
     for (let prop in serviceIncidents) {
       const incidents = [];
 
-      serviceIncidents[prop].incidents.forEach((incident) => {
+      serviceIncidents[prop].incidents.forEach(incident => {
         const assignedTo = [];
 
-        incident.assigned_to.forEach((user) => {
+        incident.assigned_to.forEach(user => {
           assignedTo.push({
             text: `${user.object.name}, ${ta.ago(new Date(Date.parse(user.at)))}`,
             href: user.object.html_url,
