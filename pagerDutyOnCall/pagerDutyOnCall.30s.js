e169eb39edca49cf420983078d8d49852c5036b1
Pedro Pablo Fuentes Schuster <pedrofuentes@gmail.com>
2016-09-20 14:52:55 -0700

    Update pagerDutyOnCall.30s.js

--- pagerDutyOnCall/pagerDutyOnCall.30s.js
+++ pagerDutyOnCall/pagerDutyOnCall.30s.js
@@ -8,6 +8,7 @@
 <bitbar.image>https://github.com/PedroFuentes/bitbar-plugins/blob/master/images/PagerDutyOnCall_Screenshot.png</bitbar.image>
 <bitbar.dependencies>node, npm/node-fetch, npm/time-ago, npm/bitbar, node/home-config</bitbar.dependencies>
 <bitbar.abouturl>https://github.com/PedroFuentes/bitbar-plugins/tree/master/pagerDutyOnCall</bitbar.abouturl>
+
 */
 'use strict';
 const fetch = require('node-fetch');
