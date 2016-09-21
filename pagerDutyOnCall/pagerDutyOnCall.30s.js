cb59684282d0b96f79fb7805b1825d511ee48df4
Pedro Fuentes <pedrofuentes@gmail.com>
2016-09-20 21:46:55 -0700

    cleaning and fixing comments

--- pagerDutyOnCall/pagerDutyOnCall.30s.js
+++ pagerDutyOnCall/pagerDutyOnCall.30s.js
@@ -4,14 +4,13 @@
 <bitbar.version>v0.1.0</bitbar.version>
 <bitbar.author>Pedro Pablo Fuentes Schuster</bitbar.author>
 <bitbar.author.github>pedrofuentes</bitbar.author.github>
-<bitbar.desc>Shows who is on call for all the active Escalation Policies and what services have active incidents.</bitbar.desc>
-<bitbar.image>https://github.com/PedroFuentes/bitbar-plugins/blob/master/images/PagerDutyOnCall_Screenshot.png</bitbar.image>
+<bitbar.desc>Shows who is on call for all the active Escalation Policies and what services have active incidents. For installation instructions check https://github.com/PedroFuentes/bitbar-plugins/blob/master/pagerDutyOnCall/README.md</bitbar.desc>
+<bitbar.image>http://cdn.pedrofuent.es/images/github/PagerDutyOnCall_Screenshot.png</bitbar.image>
 <bitbar.dependencies>node, npm/node-fetch, npm/time-ago, npm/bitbar, node/home-config</bitbar.dependencies>
 <bitbar.abouturl>https://github.com/PedroFuentes/bitbar-plugins/tree/master/pagerDutyOnCall</bitbar.abouturl>
-
-Copyright(c) 2016 Pedro Fuentes <code@pedrofuent.es>
-MIT Licensed https://opensource.org/licenses/MIT
 */
+/* MIT Licensed https://opensource.org/licenses/MIT */
+/* jshint esversion: 6 */
 'use strict';
 const fetch = require('node-fetch');
 const ta = require('time-ago')();
