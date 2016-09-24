1e7eb73226985d9ca421d877d166e6e59067160e
Pedro Fuentes <pedrofuentes@gmail.com>
2016-09-24 12:54:00 -0700

    fix depedency typo

--- pagerDutyOnCall/pagerDutyOnCall.30s.js
+++ pagerDutyOnCall/pagerDutyOnCall.30s.js
@@ -6,7 +6,7 @@
 <bitbar.author.github>pedrofuentes</bitbar.author.github>
 <bitbar.desc>Shows who is on call for all the active Escalation Policies and what services have active incidents. For installation instructions check https://github.com/PedroFuentes/bitbar-plugins/blob/master/pagerDutyOnCall/README.md</bitbar.desc>
 <bitbar.image>http://cdn.pedrofuent.es/images/github/PagerDutyOnCall_Screenshot.png</bitbar.image>
-<bitbar.dependencies>node, npm/node-fetch, npm/time-ago, npm/bitbar, node/home-config</bitbar.dependencies>
+<bitbar.dependencies>node, npm/node-fetch, npm/time-ago, npm/bitbar, npm/home-config</bitbar.dependencies>
 <bitbar.abouturl>https://github.com/PedroFuentes/bitbar-plugins/tree/master/pagerDutyOnCall</bitbar.abouturl>
 */
 /* MIT Licensed https://opensource.org/licenses/MIT */
